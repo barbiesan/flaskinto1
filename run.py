df8d380c62db5953b5127030a201605a7639709f
barbiesan <barbarasolre@gmail.com>
2022-06-06 23:53:54 +0200

    first commit

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Hello world'
+    user_dict = {
+        'username': 'barbiesan',
+        'email': 'barbiesan@gmail.com'
+        }  
+    colors = ['red','pink','green']
+    return render_template('index.html', user=user_dict, colors=colors)
 
-    @app.route("/test")
-    def test():
-        return 'this is a test!'
+@app.route('/test')
+def test():
+    return 'this is a test'
